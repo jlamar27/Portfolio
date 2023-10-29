f159cc0889b488d722d06d71119f28ff20b18195
jlamar27 <jlamar2712@gmail.com>
2023-10-29 11:32:40 -0400

    init commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,18 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
 
 function App() {
+  // component for navbar .. home about project contact (sticky)
+  // component for about me
+  // tech stack with tech logos
+  // component for each portfolio peace with link to live demo
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar />
     </div>
   );
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export default function Navbar() {
+  return (
+    <div>
+        <div className='github'>
+            <a href="http://github.com/jlamar27">github</a>
+        </div>
+        <div className='section-links'>
+            <a href="">Home</a>
+            <a href="">About</a>
+            <a href="">Projects</a>
+            <a href="">Contact</a>
+        </div>
+    </div>
+  )
+}
