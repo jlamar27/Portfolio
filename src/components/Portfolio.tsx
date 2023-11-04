f2452dfeb9bbc7e43ab83781651136bcf6595c79
jlamar27 <jlamar2712@gmail.com>
2023-11-04 00:26:59 -0400

    proj card comp made

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -1,24 +1,19 @@
 import React from 'react'
+import ProjectCard from './ProjectCard'
 
 export default function Portfolio() {
   return (
     <div className='project-screen min-h-screen ' id='project-screen'>
-      <div className='project-container w-[1024px] mx-auto h-auto flex flex-col gap-y-24 '>
-        <div className='portfolio-text mt-40 text-center '>
+      <div className='project-container w-[1024px] mx-auto h-auto flex flex-col gap-y-12 mt-32 '>
+        <div className='portfolio-text text-left '>
           <h4>Portfolio</h4>
         </div>
         <div className='project-grid grid grid-cols-1 h-full '>
-          <div className='project-display-container bg-white border-black border-solid border my-8 rounded-lg'>1
-            <div className='project-display m-5 h-56 border border-red-500 border-solid '>1.1</div>
-          </div>
-          <div className='project-display bg-white border-black border-solid border my-8 rounded-lg '>2
-            <div className='project-display m-5 h-56 border border-red-500 border-solid '>2.1</div>
-          </div>
-          <div className='project-display bg-white border-black border-solid border my-8 rounded-lg '>3
-            <div className='project-display m-5 h-56 border border-red-500 border-solid '>3.1</div>
-          </div>
+          <div className='project-display-container'>
+            <div className='project-card'><ProjectCard /></div>
         </div>
       </div>
     </div>
+  </div>
   )
 }
